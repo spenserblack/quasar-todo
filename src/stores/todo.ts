ea2cf249c6e2b890e2e0bd0d1133e0115ff26d48
Spenser Black <spenserblack01@gmail.com>
2022-10-18 14:44:48 -0400

    Add TODO

--- src/stores/todo.ts
+++ src/stores/todo.ts
@@ -1,5 +1,6 @@
 import { defineStore } from 'pinia';
 
+// TODO: If is electron, save to DB, otherwise save to localstorage
 export const todoStore = defineStore('todo', {
   state: () => ({
     todos: [],
